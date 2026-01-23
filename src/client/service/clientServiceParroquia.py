b1bfb5e36c0e1891227cdeb12d973cea332955df
starfaby <star._faby@hotmail.com>
2026-01-22 21:18:47 -0500

    Añadiendo cruce de tablas en parroquia con tipo de zona

--- src/client/service/clientServiceParroquia.py
+++ src/client/service/clientServiceParroquia.py
@@ -5,7 +5,8 @@ class ClientServiceParroquia:
     @classmethod
     def ongetClientServiceParroquia(self, id):
         try:
-            parroquiaList = pd.Series(Parroquia.query.filter(Parroquia.pfssatecparroquiaestado == 1).filter(Parroquia.pfssateccantonid == id))
+            #parroquiaList = pd.Series(Parroquia.query.filter(Parroquia.pfssatecparroquiaestado == 1).filter(Parroquia.pfssateccantonid == id))
+            parroquiaList = Parroquia.query.join(Tipozona, Parroquia.pfssatectipozonaid == Tipozona.pfssatectipozonaid).add_entity(Parroquia.pfssatecparroquianombre).add_entity(Tipozona.pfssatectipozonanombre).filter(Parroquia.pfssatecparroquiaestado == 1).filter(Parroquia.pfssateccantonid == id)
             return parroquiaList
         except SQLAlchemyError as e:
             return render('errors/error500.html', e)
--- src/static/javascript/main.js
+++ src/static/javascript/main.js
@@ -32,4 +32,6 @@ $(document).ready(function () {
         '/buscar_productos'
     );
     */
+    
+    
 })
