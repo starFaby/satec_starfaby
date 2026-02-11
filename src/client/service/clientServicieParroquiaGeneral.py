d1bc4a9960703e6056815eb4c4cbe807287e6e63
starfaby <star._faby@hotmail.com>
2026-02-10 19:36:13 -0500

    editando html de parroquia y canton

--- src/client/service/clientServicieParroquiaGeneral.py
+++ src/client/service/clientServicieParroquiaGeneral.py
@@ -6,7 +6,7 @@ class ClientServiceParroquiaGeneral:
     def ongetClientServiceParroquiaGeneral(self):
         try:
             #parroquiaList = pd.Series(Parroquia.query.filter(Parroquia.pfssatecparroquiaestado == 1).filter(Parroquia.pfssateccantonid == id))
-            parroquiaGeneralList = Parroquia.query.join(Tipozona, Parroquia.pfssatectipozonaid == Tipozona.pfssatectipozonaid).join(Canton, Parroquia.pfssateccantonid == Canton.pfssateccantonid).add_entity(Parroquia.pfssatecparroquianombre).add_entity(Tipozona.pfssatectipozonanombre).add_entity(Canton.pfssateccantonnombre).filter(Parroquia.pfssatecparroquiaestado == 1)
+            parroquiaGeneralList = Parroquia.query.join(Tipozona, Parroquia.pfssatectipozonaid == Tipozona.pfssatectipozonaid).join(Canton, Parroquia.pfssateccantonid == Canton.pfssateccantonid).join(Provincia, Canton.pfssatecprovinciaid == Provincia.pfssatecprovinciaid).add_entity(Provincia.pfssatecprovincianombre).add_entity(Parroquia.pfssatecparroquianombre).add_entity(Tipozona.pfssatectipozonanombre).add_entity(Canton.pfssateccantonnombre).filter(Parroquia.pfssatecparroquiaestado == 1)
             return parroquiaGeneralList
         except SQLAlchemyError as e:
             return render('errors/error500.html', e)
@@ -14,7 +14,7 @@ class ClientServiceParroquiaGeneral:
     @classmethod
     def ongetClientServiceParroquiaGeneralName(self, search):
         try:
-            parroquiaGeneralList = Parroquia.query.join(Tipozona, Parroquia.pfssatectipozonaid == Tipozona.pfssatectipozonaid).join(Canton, Parroquia.pfssateccantonid == Canton.pfssateccantonid).add_entity(Parroquia.pfssatecparroquianombre).add_entity(Tipozona.pfssatectipozonanombre).add_entity(Canton.pfssateccantonnombre).filter(Parroquia.pfssatecparroquianombre.like(search)).filter(Parroquia.pfssatecparroquiaestado == 1)
+            parroquiaGeneralList = Parroquia.query.join(Tipozona, Parroquia.pfssatectipozonaid == Tipozona.pfssatectipozonaid).join(Canton, Parroquia.pfssateccantonid == Canton.pfssateccantonid).join(Provincia, Canton.pfssatecprovinciaid == Provincia.pfssatecprovinciaid).add_entity(Provincia.pfssatecprovincianombre).add_entity(Parroquia.pfssatecparroquianombre).add_entity(Tipozona.pfssatectipozonanombre).add_entity(Canton.pfssateccantonnombre).filter(Parroquia.pfssatecparroquianombre.like(search)).filter(Parroquia.pfssatecparroquiaestado == 1)
             return parroquiaGeneralList
         except SQLAlchemyError as e:
             return render('errors/error500.html', e)
